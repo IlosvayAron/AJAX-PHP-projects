3f3baa092b34f44fcda4bf05f56b6bb6300fe78d
ilosvayaron <ilosvayaron@gmail.com>
2020-10-06 13:04:34 +0200

    Communicating with the Server and Sending Requests to the Server

--- Food ordering website/foodstore.js
+++ Food ordering website/foodstore.js
@@ -22,3 +22,14 @@ function creatXmlHttpRequestObject(){
     else
         return xmlHttp;
 }
+
+function process(){
+    if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
+        food = encodeURIComponent(document.getElementById("userInput").value);
+        xmlHttp.open("GET","foodstore.php?food=",+food,true);
+        xmlHttp.onreadystatechange = handleServerResponse;
+        xmlHttp.send(null);
+    }else{
+        setTimeout('process()',1000);
+    }
+}
